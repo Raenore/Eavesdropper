67a6020e81c02aa5d584eb195ac23e3df474f1bc
Raenore <172234435+Raenore@users.noreply.github.com>
2026-03-24 04:34:08 +0100

    Update Config.lua

--- Core/Config.lua
+++ Core/Config.lua
@@ -45,7 +45,7 @@ local function SetupSounds()
 	end
 end
 
----Displays the configuration menu for the addon
+---Display the configuration menu for the addon.
 ---@param frame table
 ---@param dedicatedFrame boolean?
 ---@param groupFrame boolean?
